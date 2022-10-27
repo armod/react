ffaaf0e77d5ae16e78a294794c07e8bf632989f0
armod <armods@gmail.com>
2022-10-27 09:52:48 +0200

    reducer

--- userecucer-test/src/App.js
+++ userecucer-test/src/App.js
@@ -1,5 +1,28 @@
+import { useState } from 'react'
+import { data } from './data'
+
+const reducer = {}
 function App() {
-  return <div></div>
+  const [people, setPeople] = useState(data)
+
+  return (
+    <section className='wrapper'>
+      <h2>Persons</h2>
+      {people.map((person) => {
+        return (
+          <ul key={person.id}>
+            <li>
+              <h3>{person.name}</h3>
+              <h4>{person.age}</h4>
+              <p>{person.lang}</p>
+              <button className='btn'>remove</button>
+            </li>
+          </ul>
+        )
+      })}
+      <button className='btn'>remove all</button>
+    </section>
+  )
 }
 
 export default App
---
+++ userecucer-test/src/data.js
@@ -0,0 +1,9 @@
+export const data = [
+  { id: 1, name: 'Maciej', age: 38, lang: 'javascript' },
+  { id: 2, name: 'Tomek', age: 38, lang: 'java' },
+  { id: 3, name: 'Bartek', age: 38, lang: 'c++' },
+  { id: 4, name: 'Paweł', age: 38, lang: 'c#' },
+  { id: 5, name: 'Łukasz', age: 38, lang: 'javascript' },
+  { id: 6, name: 'Michał', age: 38, lang: 'c++' },
+  { id: 7, name: 'Jacek', age: 38, lang: 'javascript' },
+]

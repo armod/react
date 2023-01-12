aef20d6f8dbe9fba49d339e19592c1de658bbfb9
armod <armods@gmail.com>
2023-01-12 08:14:27 +0100

    update

--- random-website/src/App.js
+++ random-website/src/App.js
@@ -1,10 +1,22 @@
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components/macro'
 import Home from './pages/Home'
 
 function App() {
+  const [data, setData] = useState('')
+  const getData = async () => {
+    const resp = await fetch('https://api.sampleapis.com/coffee/hot')
+    const json = await resp.json()
+    setData(json)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
   return (
     <Wrapper>
       <Home />
+      {console.log(JSON.stringify(data, null, 2))}
     </Wrapper>
   )
 }

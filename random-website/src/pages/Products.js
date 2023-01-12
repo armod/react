25ef0bc774eeb0d1100be862d31ce3f7e7d8e049
armod <armods@gmail.com>
2023-01-12 07:31:36 +0100

    update

---
+++ random-website/src/pages/Products.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Products = () => {
+  return <div>Products</div>
+}
+
+export default Products
+
+const Wrapper = styled.section`
+  display: flex;
+`

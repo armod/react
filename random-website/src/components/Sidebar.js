8e512ce345161fef44a604f4e5f1250bdfb806c8
armod <armods@gmail.com>
2023-01-13 07:26:49 +0100

    update

---
+++ random-website/src/components/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useGlobalContext } from '../context'
+
+const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
+  return <div>Sidebar</div>
+}
+
+export default Sidebar
--- random-website/src/pages/Home.js
+++ random-website/src/pages/Home.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
+import { useGlobalContext } from '../context'
 
 const Home = () => {
-  return <div>Home</div>
+  const { closeSubmenu } = useGlobalContext()
+  return (
+    <Wrapper>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Temporibus, dolores.
+          </h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
+            delectus placeat unde nobis eveniet exercitationem iste, repellendus
+            quaerat molestias enim numquam, totam praesentium earum repudiandae
+            pariatur architecto maiores ab dolor accusamus odit repellat officia
+            cupiditate provident. Unde, soluta iste. Quam a ipsam doloribus aut
+            cum commodi quaerat assumenda exercitationem placeat.
+          </p>
+        </article>
+        <article className='hero-images'>
+          <img src='' alt='' />
+        </article>
+      </div>
+    </Wrapper>
+  )
 }
 
 export default Home
+
+const Wrapper = styled.section`
+  display: flex;
+`

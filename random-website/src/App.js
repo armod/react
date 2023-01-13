608ad7329cf634699ff95b8d20f453967dfefa07
armod <armods@gmail.com>
2023-01-13 07:16:01 +0100

    update

--- random-website/src/App.js
+++ random-website/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components/macro'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
   }, [])
   return (
     <Wrapper>
+      <Navbar />
       <Home />
       {/* {console.log(JSON.stringify(data, null, 2))} */}
     </Wrapper>
--- random-website/src/components/Navbar.js
+++ random-website/src/components/Navbar.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import { useGlobalContext } from '../context'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext()
+  return (
+    <nav className='nav'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src='' alt='' />
+          <button className='btn toggle-btn' onClick={openSidebar}>
+            ikona
+          </button>
+        </div>
+        <ul className='nav-links'>
+          <li>
+            <button className='link-btn'>button1</button>
+          </li>
+          <li>
+            <button className='link-btn'>button2</button>
+          </li>
+          <li>
+            <button className='link-btn'>button3</button>
+          </li>
+          <li>
+            <button className='link-btn'>button4</button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
